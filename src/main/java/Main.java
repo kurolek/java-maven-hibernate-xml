ff05b51590027978a9b302bb5221b6ad6bf6d72e
kurolek <oleksiukoleh@ukr.net>
2020-06-17 02:21:13 +0200

    Added weapon entity

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -11,9 +11,22 @@ public class Main {
         u.setFirstName("f1");
         u.setLastName("l1");
         u.setAge(20);
+
         EntityManager em = sessionFactory.createEntityManager();
         em.getTransaction().begin();
         em.persist(u);
         em.getTransaction().commit();
+
+
+
+        Weapon w = new Weapon();
+        w.setName("Sword of power");
+        w.setDamage(7);
+        w.setType("Sword");
+
+        em.getTransaction().begin();
+        em.persist(w);
+        em.getTransaction().commit();
+
     }
 }
